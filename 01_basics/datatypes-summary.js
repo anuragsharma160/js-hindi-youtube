ea9cddad51d228d4e08c836e2a320e7cb665f7cd
anuragsharma160 <121424453+anuragsharma160@users.noreply.github.com>
2024-11-12 13:31:53 +0000

    Stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -65,7 +65,7 @@ const anotherId = Symbol('123')
 // milega, id aur anotherId ki value same nahi hogi
 // verify kar lete hai kya id equal hai anotherId k as follows
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // above ka o/p false, halaki values humne same li hai i.e. ('123') par jo return value hume mila
 // hai wo same nahi hai
@@ -115,22 +115,91 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
-console.log(typeof outsideTemp);
+// console.log(typeof outsideTemp);
 
-console.log(typeof scoreValue);
+// console.log(typeof scoreValue);
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
 // for above -- function actual mei function hi return karta hai, but isko bola jata hai object function
 // to jo yeh jitne bhi non primitive type hai inka jo return type ata hai wo object hi ata hai
 
-console.log(typeof heros);
+// console.log(typeof heros);
 
-console.log(typeof id);
+// console.log(typeof id);
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 // assignment - har datatype ka return type likhna hai
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Two types of memories are there as follows
+// Stack, Heap
+// Jitne bhi PRIMITIVE datatype bataye the wahan sab jagah STACK memory use hoti hai
+// Jitne bhi NON-PRIMITIVE datatype bataye the wahan sab jagah HEAP memory use hoti hai
+
+// Itna yaad rakhna ki jab bhi STACK memory use hoti hai, iska matlab jo bhi aapne variable
+// declare kara hai uska apko ek copy milta hai
+
+// Jab bhi ek memory HEAP k andar define hoti hai ya koi object ya kuch bhi HEAP k andar chala
+// jata hai to wahan se apko milta hai reference original value ka to jo bhi change karenge wo
+// original value mei change hoga
+
+// Eg as follows
+
+// let myYoutubename = "hiteshchoudharydotcom"
+
+// myYoutubename ek variable hai of primitive type, now iske andar values change hoti rehti hai
+// (variable k andar may be) , now eg of kis tareh se hum value ko change karte hai to hum ek 
+// value lete hai as follows
+
+// let anothername = myYoutubename
+
+// to ab kya hoga iss anothername k andar jo bhi value hogi wo kya hai -- hiteshchoudharydotcom
+
+// console.log(anothername);
+
+// above ka o/p hiteshchoudharydotcom
+
+// lekin ab hum anothername k andar value change kar dete hai as line number 176 of same eg (as above) as follows
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+// to abhi humne anothername k andar value change kar di, to aap soch rahe honge ki anothername
+// k andar abhi value kya hai -- chaiaurcode hai , lekin yeh jo reference hai humne 
+// to isi ka diya tha (hiteshchoudharydotcom ka or may be myYoutubename ka)
+// to myYoutubename k andar kya value hogi , yahi p interesting concept hai to hum dekhte hai ki
+// myYoutubename k andar kya value hai and anothername k andar kya value hai
+// myYoutubename k andar purani hi value hai --- hiteshchoudharydotcom aur
+// anothername hai uski value change ho gayi as --- chaiaurcode
+
+// now advanced datatype (object) lete hai taki hume value samajh mei aa jaye as follows
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// above both ka o/p hitesh@google.com
+
