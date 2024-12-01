f3c00c9a4831c0e556b3e9c14c6051056fb0bc98
anuragsharma160 <121424453+anuragsharma160@users.noreply.github.com>
2024-12-01 16:25:03 +0000

    Array Part 2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -87,4 +87,73 @@ console.log(real_another_array);
 //   5
 // ]
 
-// 11:45
+// ======================================================================================================
+
+// ek aur cheej jo kaam ayegi especially jab hum data scrapping kar rahe hai, to jab bhi hum data select
+// karte hai kisi bhi webpage se to wo kai bar alag format mei ata hai nodelist mei ata hai, ya objects mei ata hai
+// ya fir string mei aya but chahiye to array to array k upar hi loop kar sakte hai , to wahan pe aap array ko
+// use kar sakte hai iss tareh se as follows
+// actually mei aap poch bhi sakte hai aur usko convert bhi kar sakte hai jaise suppose kariye hum pochte hai
+// is array as follows
+
+// Array.isArray()
+// hum iss array k andar ek hitesh naam ki string de dete hai
+
+// Array.isArray("Hitesh")
+// isko hum console log karke dekh lete hai
+
+console.log(Array.isArray("Hitesh"))
+// to phele hum poch rahe hai isse question ki batao yeh jo cheej hai aapke pas yeh array hai kya
+// o/p false, means yeh array nahi hai
+
+// lekin agar convert karna hota to hum .isArray ki jagah likh dete .from , means hum isko bana dete hai
+// ek array .from se as follows
+
+console.log(Array.from("Hitesh"))
+// o/p [ 'H', 'i', 't', 'e', 's', 'h' ]
+// to isne pora hi array bana diya hai, obvious si baat hai jo bhi values aap denge usko yeh array k andar convert
+// kar dega, aap chahe to objects bhi de sakte hai, strings bhi de sakte hai , to jaise bhi wo array convert kar payega
+// wo array bana dega iss tareh se
+
+// hum isme aur bhi values de sakte hai , jaise humne diya isko ek object {} as follows
+
+// console.log(Array.from({}))
+
+// and humne object {} k andar likha name: "hitesh"
+
+console.log(Array.from({name: "hitesh"}))
+// hitesh string mei diya , aur bhi values de sakte hai
+// o/p is []
+// to isne o/p mei diya ek empty array, kyuki isko {name: "hitesh"} directly yeh convert nahi kar paa raha
+
+// to apko bolna padega, phele actually mei dena padega ki achha keys ka mai array banau ya fir mai sirf
+// iske values ka array banau (key value pair) , agar yeh nahi bana payega to humesha kya dega 
+// empty array dega []
+// to iska console.log(Array.from({name: "hitesh"})) dhayan rakhna , yeh interesting case hai interviews k liye
+// 
+
+// =============================================================================================
+
+// jaise suppose kariye apke pas kuch values hai
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+// aisi situations bhi ayegi apke pas mei ki apke pas multiple variables hai ya fir arrays hai unko bhi
+// apko array mei convert karna hai to bhi aap easily kar sakte hai
+// instead .from ki jagah .of kar sakte hai
+
+// console.log(Array.of);
+// jaise hi hum .of karenge to yeh kya karta hai -- returns a new array from a set of elements
+// ab set of elements kuch bhi ho sakte hai variables bhi ho sakte hai, arrays bhi ho sakte hai
+// to iske andar jitne bhi elements hai jin ko convert karna de dijiye
+
+
+console.log(Array.of(score1, score2, score3));
+
+// o/p [ 100, 200, 300 ]
+
+// Read more about .isArray, .from and .of
+
+
